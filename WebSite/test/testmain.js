2964c21e207b469f6edc3de4f3062fb5d9d265cf
JIu04 <124769813+Jiu125@users.noreply.github.com>
2024-01-15 17:23:04 +0900

    Delete WebSite directory

--- WebSite/test/testmain.js
+++
@@ -1,8 +0,0 @@
-const toogleBtn = document.querySelector('.navbar_toogleBtn');
-const meau = document.querySelector('.navbar_meau');
-const search = document.querySelector('.navbar_search');
-
-toogleBtn.addEventListener('click', () => {
-    meau.classList.toggle('active');
-    search.classList.toggle('active');
-});
